7910aaa8f893a56955f818be338d8b129f2130c9
Michael Vincent <mvincent1212@utexas.edu>
2021-12-05 13:13:21 -0500

    disable es-lint

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,7 @@ function Contact() {
     message: ''
   })
   const [invalidEmail, setInvalidEmail] = useState(false);
+  //eslint-disable-next-line
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   function sendEmail(e) {
