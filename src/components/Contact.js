7955807b3c534691afdcda9880c74b71cc0debbc
Michael Vincent <mvincent1212@utexas.edu>
2021-12-05 13:08:38 -0500

    added check for valid email and state to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,19 @@ import "../styles/contact.css";
 import emailjs from "emailjs-com";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Contact() {
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   function sendEmail(e) {
+    e.preventDefault();
     emailjs
       .sendForm(
         "service_7769zcq",
@@ -18,6 +27,12 @@ function Contact() {
         console.log(res);
       })
       .catch((err) => console.log(err));
+
+      const formSent = document.querySelector('.form-submitted');
+      formSent.innerHTML = "Message sent!";
+
+      setInputValues({name: '', email: '', message: ''});
+      setInvalidEmail(() => false);
   }
 
   useEffect(() => {
@@ -42,16 +57,30 @@ function Contact() {
             <input
               type="text"
               name="name"
+              value={inputValues.name}
               placeholder="Michael Vincent"
+              onChange = {(e) => {
+                setInputValues({...inputValues, name: e.target.value})
+              }}
             ></input>
           </div>
           <div class="email-container">
             <label>email</label>
             <input
-              type="text"
+              type="email"
               name="email"
+              value={inputValues.email}
               placeholder="michaelvincent@example.com"
+              onChange = {(e) => {
+                setInputValues({...inputValues, email: e.target.value});
+                if (!regex.test(inputValues.email)) {
+                  setInvalidEmail(() => true);
+                } else if (invalidEmail === true) {
+                  setInvalidEmail(() => false);
+                }
+              }}
             ></input>
+            <span className="invalid-text">{invalidEmail ? "Please enter a valid email" : ""}</span>
           </div>
           <div class="message-container">
             <label>message</label>
@@ -59,12 +88,17 @@ function Contact() {
               name="message"
               rows="8"
               placeholder="Hi, let's work together!"
+              value={inputValues.message}
+              onChange = {(e) => {
+                setInputValues({...inputValues, message: e.target.value})
+              }}
             ></textarea>
           </div>
           <div class="submit-button">
             <input type="submit" value="Send"></input>
           </div>
         </form>
+        <div className='form-submitted'></div>
       </div>
     </motion.div>
   );
